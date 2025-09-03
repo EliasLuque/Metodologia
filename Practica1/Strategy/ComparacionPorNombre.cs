152514e718cedb32a2c2ddd7d1802019a78e887d
LuqueElias <eliasluque41@gmail.com>
2025-09-03 13:47:57 -0300

    Corrección

--- Practica1/Strategy/ComparacionPorNombre.cs
+++ Practica1/Strategy/ComparacionPorNombre.cs
@@ -1,6 +1,6 @@
 public class ComparcionPorNombre : EstrategiaDeComparacion
 {
-    public bool sosIgual(Alumno a1, Alumno a2) => a1.getNombre() == a2.getNombre();
-    public bool sosMenor(Alumno a1, Alumno a2) => a1.getNombre < a2.getNombre();
-    public bool sosMayor(Alumno a1, Alumno a2) => a1.getNombre > a2.getNombre();
+    public bool sosIgual(Alumno a1, Alumno a2) => string.Compare(a1.getNombre(), a2.getNombre()) == 0;
+    public bool sosMenor(Alumno a1, Alumno a2) => string.Compare(a1.getNombre(), a2.getNombre()) < 0;
+    public bool sosMayor(Alumno a1, Alumno a2) => string.Compare(a1.getNombre(), a2.getNombre()) > 0;
 }
