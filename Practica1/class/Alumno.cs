64e3e9ced576b727ddcd40dd91a6807b0c5cd827
LuqueElias <eliasluque41@gmail.com>
2025-09-21 14:37:15 -0300

    Ejercicios 1 y 2

--- Practica1/class/Alumno.cs
+++ Practica1/class/Alumno.cs
@@ -3,6 +3,7 @@ public class Alumno : Persona, IObservador
     private int legajo;
     private double promedio;
     private EstrategiaDeComparacion estrategia;
+    private double calificaion;
 
     public Alumno(string n, int d, int l, double p) :
     base(n, d)
@@ -16,12 +17,14 @@ public class Alumno : Persona, IObservador
     public int getLegajo() => this.legajo;
     public double getPromedio() => this.promedio;
     public void cambiarEstrategia(EstrategiaDeComparacion e) => this.estrategia = e;
+    public double getCalificacion() => this.calificaion;
+    public void setCalificacion(double cal) => this.calificaion = cal;
+    public virtual int responderPregunta(int pregunta) => new GeneradorDeDatosAleatorios().numeroAleatorio(3) + 1;
+    public string mostrarCalificacion() => $"{this.getNombre}\t{this.calificaion}";
 
     // Interface Comparable
     public override bool sosIgual(Comparable c) => this.estrategia.sosIgual(this, (Alumno)c);
-
     public override bool sosMayor(Comparable c) => this.estrategia.sosMayor(this, (Alumno)c);
-
     public override bool sosMenor(Comparable c) => this.estrategia.sosMenor(this, (Alumno)c);
 
     // Interface Obervador
@@ -34,17 +37,14 @@ public class Alumno : Persona, IObservador
             case 0:
                 Console.WriteLine("Mirando el celular");
                 break;
-                
             case 1:
                 Console.WriteLine("Dibujando en el margen de la carpeta");
                 break;
-            
             case 2:
                 Console.WriteLine("Tirando aviones de papel");
                 break;
         }
     }
-
     public void actualizar(IObservado observado)
     {
         if (((Profesor)observado).getHablando())
---
+++ Practica1/class/AlumnoMuyEstudioso.cs
@@ -0,0 +1,11 @@
+public class AlumnoMuyEstudioso : Alumno
+{
+    public AlumnoMuyEstudioso(string n, int d, int l, double p) :
+    base(n, d, l, p)
+    { }
+
+    public override int responderPregunta(int pregunta)
+    {
+        return base.responderPregunta(pregunta);
+    }
+}
