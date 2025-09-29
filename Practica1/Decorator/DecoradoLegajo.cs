5bd5c27136c15c700e51da9f2105199b322a4825
LuqueElias <eliasluque41@gmail.com>
2025-09-29 18:25:18 -0300

    rm deco

--- Practica1/Decorator/DecoradoLegajo.cs
+++
@@ -1,7 +0,0 @@
-public class DecoradoLegajo : AlumnoDecorator
-{
-    public DecoradoLegajo(IAlumno alumno) : base(alumno)
-    { }
-    public string mostrarCalificacion() => base.mostrarCalificacion();
-    
-}
